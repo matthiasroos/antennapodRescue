0ac58f86795770382e246247f89c790dc9793908
Matthias Roos <matthias.roos@freenet.de>
2021-08-31 19:58:18 +0200

    Use join, add additional function

--- media.py
+++ media.py
@@ -1,5 +1,6 @@
 import typing
 
+import pandas as pd
 import sqlmodel
 
 import models
@@ -14,6 +15,26 @@ def get_media_from_db(sqlite_filename: str, feed_id: int) -> typing.List[models.
     """
     engine = sqlmodel.create_engine(f'sqlite:///{sqlite_filename}')
     with sqlmodel.Session(engine) as session:
-        statement = sqlmodel.select(models.FeedMedia)
+        statement = sqlmodel.select(models.FeedMedia)\
+            .filter(models.FeedMedia.feeditem == models.FeedItem.id).where(models.FeedItem.feed == feed_id)
         media = session.exec(statement).all()
     return media
+
+
+def get_media_df_from_db(sqlite_filename: str, feed_id: int) -> pd.DataFrame:
+    """
+
+    :param sqlite_filename:
+    :param feed_id:
+    :return:
+    """
+    engine = sqlmodel.create_engine(f'sqlite:///{sqlite_filename}')
+    con = engine.connect()
+    statement = sqlmodel.select(models.FeedMedia) \
+        .filter(models.FeedMedia.feeditem == models.FeedItem.id).where(models.FeedItem.feed == feed_id)
+    media_df = pd.read_sql(
+        sql=statement,
+        con=con,
+        columns=['id', 'duration', 'download_url', 'downloaded', 'filesize', 'feeditem'])
+    con.close()
+    return media_df

5eda2cce336608e6a060df1219215c49e28a4268
Matthias Roos <matthias.roos@freenet.de>
2021-09-07 12:29:44 +0200

    Add FeedMedia class based on sqlalchemy

--- sqlalchemy_/models.py
+++ sqlalchemy_/models.py
@@ -28,6 +28,25 @@ class EpochTimestamp(sqlalchemy.types.TypeDecorator):
         return datetime.datetime.utcfromtimestamp(value / 1000)
 
 
+class Milliseconds(sqlalchemy.types.TypeDecorator):
+    """
+    Type extension for Integer as Milliseconds
+    """
+    impl = sqlalchemy.types.Integer
+
+    def process_bind_param(self, value, dialect):
+        """
+        Manipulate input parameter before writing it to the database.
+        """
+        return value.milliseconds
+
+    def process_result_value(self, value, dialect):
+        """
+        Convert value coming as result from the db.
+        """
+        return datetime.timedelta(milliseconds=value)
+
+
 class Feed(Base):
     """
     Table Feeds
@@ -58,3 +77,19 @@ class FeedItem(Base):
 
     item_identifier = sqlalchemy.Column(sqlalchemy.String)
     image_url = sqlalchemy.Column(sqlalchemy.String)
+
+
+class FeedMedia(Base):
+    """
+    Table FeedMedia
+    """
+    __tablename__ = "FeedMedia"
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    duration: datetime.timedelta = sqlalchemy.Column(Milliseconds)
+    download_url = sqlalchemy.Column(sqlalchemy.String)
+    downloaded = sqlalchemy.Column(sqlalchemy.Integer)
+    filesize = sqlalchemy.Column(sqlalchemy.Integer)
+    playback_completion_date: int = sqlalchemy.Column(sqlalchemy.Integer)
+    feeditem = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('FeedItems.id'))
+    played_duration: int = sqlalchemy.Column(sqlalchemy.Integer)
+    last_played_time: int = sqlalchemy.Column(sqlalchemy.Integer)

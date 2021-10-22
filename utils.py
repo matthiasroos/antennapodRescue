d47da042181f574910308508667119aa88b31a63
Matthias Roos <matthias.roos@freenet.de>
2021-10-22 22:03:16 +0200

    Add simple doctest

--- utils.py
+++ utils.py
@@ -5,6 +5,12 @@ import xml.etree.ElementTree
 def parse_pubdate(element: xml.etree.ElementTree.Element):
     """
 
+    >>> item = xml.etree.ElementTree.Element('item')
+    >>> f = xml.etree.ElementTree.SubElement(item, 'pubDate')
+    >>> f.text = 'Sun, 17 Oct 2021 11:00:32 GMT'
+    >>> parse_pubdate(element=item)
+    1634461232000
+
     :return:
     """
     pubdate = element.find('pubDate').text
@@ -16,4 +22,4 @@ def parse_pubdate(element: xml.etree.ElementTree.Element):
         datetime_format_2 = '%a, %d %b %Y %H:%M:%S %Z'
         datetime_entry = datetime.datetime.strptime(pubdate, datetime_format_2).timestamp() * 1000
 
-    return datetime_entry
+    return int(datetime_entry)

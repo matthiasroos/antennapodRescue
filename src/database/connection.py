006076f524ecf4098769579369dab3c70db287b2
Matthias Roos <matthias.roos@freenet.de>
2022-06-17 13:42:13 +0200

    Add aiosql to general get_connection function

--- src/database/connection.py
+++ src/database/connection.py
@@ -1,3 +1,4 @@
+import src.orm.aiosql.database
 import src.orm.peewee.database
 import src.orm.sqlalchemy.database
 import src.orm.sqlmodel.database
@@ -18,6 +19,8 @@ def get_connection(orm_model: str,
         connection = src.orm.sqlmodel.database.get_connection(sqlite_filename=sqlite_filename)
     elif orm_model == 'peewee':
         connection = src.orm.peewee.database.get_connection(sqlite_filename=sqlite_filename)
+    elif orm_model == 'aiosql':
+        connection = src.orm.aiosql.database.get_connection(file_name=sqlite_filename)
     else:
         connection = src.orm.sqlalchemy.database.get_connection(sqlite_filename=sqlite_filename)
 

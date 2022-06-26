748ab4e75a76c76c90a13fb89f9f96a79cae667b
Matthias Roos <matthias.roos@freenet.de>
2022-06-26 17:09:19 +0200

    Add FeedItem class and fetch function using pony ORM

--- src/orm/pony/database.py
+++ src/orm/pony/database.py
@@ -4,6 +4,7 @@ import typing
 import pandas as pd
 import pony.orm
 
+import src.database.feeditems
 import src.database.feeds
 import src.orm.pony.models
 
@@ -15,8 +16,10 @@ def get_connection(file_name: str) -> sqlite3.Connection:
     :return:
     """
     db = src.orm.pony.models.db
+
     try:
         db.bind(provider='sqlite', filename=file_name)
+        db.provider.converter_classes.append((src.orm.pony.models.Epoch, src.orm.pony.models.EpochConverter))
         db.generate_mapping()
     except pony.orm.core.BindingError:
         pass
@@ -37,6 +40,21 @@ def fetch_feeds() -> pd.DataFrame:
     return feeds_df
 
 
+def fetch_feeditems(where_cond) -> pd.DataFrame:
+    """
+
+    :param where_cond:
+    :return:
+    """
+    query = pony.orm.select(feeditem for feeditem in src.orm.pony.models.FeedItem
+                            if getattr(feeditem, list(where_cond.keys())[0]) == list(where_cond.values())[0])
+    columns = src.database.feeditems.get_feeditems_standard_columns()
+    feeditems = [[getattr(f, col) for col in columns] for f in query]
+    feeditems_df = pd.DataFrame(feeditems, columns=columns)
+
+    return feeditems_df
+
+
 def fetch_kind(kind: str,
                connection: sqlite3.Connection,
                where_cond: typing.Dict[str, typing.Any] = None,
@@ -50,6 +68,9 @@ def fetch_kind(kind: str,
     """
     if kind == 'feeds':
         elements_df = fetch_feeds()
+
+    elif kind == 'feeditems':
+        elements_df = fetch_feeditems(where_cond=where_cond)
     else:
         elements_df = pd.DataFrame()
     return elements_df
--- src/orm/pony/models.py
+++ src/orm/pony/models.py
@@ -1,12 +1,48 @@
+import datetime
+
 import pony.orm
+import pony.orm.dbapiprovider
 
 
 db = pony.orm.Database()
 
 
-class Feed(db.Entity):
+class Epoch:
+    """
+    Epoch class
+    """
+    pass
+
+
+class EpochConverter(pony.orm.dbapiprovider.IntConverter):
+    """
+    Converter for Integer as Epoch
     """
 
+    def validate(self, val):
+        """
+
+        :param val:
+        :return:
+        """
+        return val
+
+    def py2sql(self, val):
+        """
+        Manipulate input parameter before writing it to the database.
+        """
+        return val.replace(tzinfo=datetime.timezone.utc).timestamp() * 1000
+
+    def sql2py(self, value):
+        """
+        Convert value coming as a result from the db.
+        """
+        return datetime.datetime.utcfromtimestamp(value / 1000)
+
+
+class Feed(db.Entity):
+    """
+    Table Feeds
     """
     _table_ = 'Feeds'
 
@@ -15,3 +51,22 @@ class Feed(db.Entity):
     file_url = pony.orm.Required(str)
     download_url = pony.orm.Required(str)
     downloaded = pony.orm.Required(str)
+
+
+class FeedItem(db.Entity):
+    """
+    Table FeedItems
+    """
+    _table_ = 'FeedItems'
+
+    id = pony.orm.PrimaryKey(int)
+    title = pony.orm.Required(str)
+    pubDate = pony.orm.Required(Epoch)
+    read = pony.orm.Required(int)
+    link = pony.orm.Required(str)
+    description = pony.orm.Required(str)
+
+    feed = pony.orm.Required(int)
+
+    item_identifier = pony.orm.Required(str)
+    image_url = pony.orm.Optional(str)

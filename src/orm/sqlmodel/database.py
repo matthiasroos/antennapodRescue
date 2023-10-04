691b7840fd3982e208465bd9d2e46e6689b012c3
Matthias Roos <matthias.roos@freenet.de>
2023-10-04 21:31:59 +0200

    Add sqlmodel execute_statements function

--- src/orm/sqlmodel/database.py
+++ src/orm/sqlmodel/database.py
@@ -1,3 +1,5 @@
+import typing
+
 import sqlalchemy.engine
 import sqlmodel
 
@@ -18,3 +20,20 @@ def get_connection(sqlite_filename: str) -> sqlalchemy.engine.Connection:
     :return:
     """
     return get_engine(sqlite_filename=sqlite_filename).connect()
+
+
+def execute_statements(sqlite_filename: str,
+                       statements: typing.List):
+    """
+
+    :param sqlite_filename:
+    :param statements:
+    :return:
+    """
+    connection = get_connection(sqlite_filename=sqlite_filename)
+    with sqlmodel.Session(connection) as session:
+        results = []
+        for statement in statements:
+            result = session.exec(statement)
+            results.append(result)
+    return results
